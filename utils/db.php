f59e2a84b878ab8829b0a9b5ffdca620e14a1ac2
devkcud <patommmmm@proton.me>
2024-03-25 19:30:42 -0300

    remove vars

--- utils/db.php
+++ utils/db.php
@@ -1,10 +1,5 @@
 <?php
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "portfolio";
-
-$db = new mysqli($hostname, $username, $password, $database);
+$db = new mysqli("localhost", "root", "", "portfolio");
 
 if ($db->connect_error) {
     die("ERROR: $db->connect_error");
