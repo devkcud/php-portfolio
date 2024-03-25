46b2b3b8957f37f2a07bae324cb63ebed8fce03b
devkcud <patommmmm@proton.me>
2024-03-25 19:43:56 -0300

    Reformat

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
-<?php require_once("./utils/gen.php"); gen_head("Home", 0) ?>
+<?php
+require_once("./utils/gen.php");
+gen_head("Home", 0);
+?>
 
-<section class="flex items-center justify-center w-full min-h-screen">
-  <a href="./pages/new.php" class="btn">Criar um portfolio</a>
+<section class="flex flex-col items-center justify-center w-full min-h-screen gap-4">
+    <h1 class="text-5xl">Olá, mundo!</h1>
+
+    <a href=" ./pages/new.php" class="btn">Criar um portfolio</a>
 </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['**/*.html', '**/*.php'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['**/*.html', '**/*.php'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
